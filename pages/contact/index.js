1aa874378391c7bd5458df503a35a09741f819cb
Jani <jani.behm@gmail.com>
2022-06-06 22:40:34 +0300

    lomake border fix

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -10,6 +10,9 @@ function IndexTest() {
         src="https://docs.google.com/forms/d/e/1FAIpQLSfbWItMnUtgCmmz3nRcwuZ_VnLZ-vj0KCcluAUx2V9PqcXaOw/viewform?embedded=true"
         width="640"
         height="935"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
       >
         Ladataan…
       </iframe>
