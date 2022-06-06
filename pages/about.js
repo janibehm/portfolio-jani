3fb15a716dac400d398342004f62ae3db9914a19
Jani <jani.behm@gmail.com>
2022-06-06 17:22:11 +0300

    fix the build issue

--- pages/about.js
+++ pages/about.js
@@ -20,24 +20,28 @@ const about = () => {
          web-sivut tai web kauppa. WordPress (myös WordPress.org) on alun perin blogien luomiseen ja ylläpitoon tarkoitettu, avoimeen lähdekoodin WWW-sisällönhallintaohjelmisto. WordPress on kirjoitettu PHP-kielellä ja se käyttää tietojen tallentamiseen MySQL-tietokantaa.
          Tammikuussa 2015 internetin 10 miljoonasta suosituimmasta sivustosta 23,3 prosenttia käytti WordPressiä. North Patrolin tutkimuksen mukaan 40 % oppilaitoksista käyttää WordPressiä. Se on suosituin WWW-sisällönhallintajärjestelmä.</p> 
         <div>
-        <Image src="/wordpress_logo.jpg" width={470} height={270}/>
+        <Image src="/wordpress_logo.jpg" alt="wp_logo" width={470} height={270}/>
         </div>
         <div>
 
         <h3>React ja Nextjs</h3>
-        <p className={styles.about }> React on fron-end javascipt kirjasto, joka mahdollistaa UI komponenteilla web-sivun rakentamisen. Sitä käytetään yleensä "singlepage" tai mobiili ohjelmistojen tekemsiin.
-        Next.js on kehitys ympäristö, joka on rakenettu Node.js päälle, joka mahdollistaa React pohjaisten ohjelmistojen käyttää serveri puolen rendausta, sekä genereoida staattisia websivuja.</p>
-        <div className='logo-container'>
-        
-            <Image src="/react_logo.png" width={240} height={240}/>
-         
-            <div className='next-js-logo'>
-            <Image src="/next-js_logo.png" width={240} height={240}/>
+        <p className={styles.about }> Sopivat tilanteeseen, jos halutaan yksityiskohtaiset sivut, sekä
+        enemmän hallintaa ja nopeutta.</p>
+
+            <Image src="/next-js_logo.png" alt="nextjs_logo" width={240} height={240}/>
             </div>
             </div>
-    
+        <h3>Wordpress</h3>
+        <p className={styles.about }>
+         Hyvä ratkaisu projektissa, jossa halutaan käyttää valmiita tai custom teemoja, sekä plugineja luomaan 
+        web-sivut tai web kauppa.</p> 
+        <div>
+        <Image src="/wordpress_logo.jpg" alt="react_logo"  width={470} height={270}/>
         </div>
+        <div>
+
         </div>
+   
         
         </>
    
