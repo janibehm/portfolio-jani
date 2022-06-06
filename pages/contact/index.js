9372c59487eb00da960030b3bb604d83eda1f263
Jani <jani.behm@gmail.com>
2022-06-06 22:34:30 +0300

    build fix

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -10,9 +10,6 @@ function IndexTest() {
         src="https://docs.google.com/forms/d/e/1FAIpQLSfbWItMnUtgCmmz3nRcwuZ_VnLZ-vj0KCcluAUx2V9PqcXaOw/viewform?embedded=true"
         width="640"
         height="935"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
       >
         Ladataan…
       </iframe>
