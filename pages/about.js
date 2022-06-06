34b78ec0cafd25544f84096f21c5d88d8bcfd592
Jani <jani.behm@gmail.com>
2022-06-06 04:13:30 +0300

    fix teknologiat content

--- pages/about.js
+++ pages/about.js
@@ -25,8 +25,7 @@ const about = () => {
         <div>
 
         <h3>React ja Nextjs</h3>
-        <p className={styles.about }> 
-        React on fron-end javascipt kirjasto, joka mahdollistaa UI komponenteilla web-sivun rakentamisen. Sitä käytetään yleensä "singlepage" tai mobiili ohjelmistojen tekemsiin.
+        <p className={styles.about }> React on fron-end javascipt kirjasto, joka mahdollistaa UI komponenteilla web-sivun rakentamisen. Sitä käytetään yleensä "singlepage" tai mobiili ohjelmistojen tekemsiin.
         Next.js on kehitys ympäristö, joka on rakenettu Node.js päälle, joka mahdollistaa React pohjaisten ohjelmistojen käyttää serveri puolen rendausta, sekä genereoida staattisia websivuja.</p>
         <div className='logo-container'>
         
