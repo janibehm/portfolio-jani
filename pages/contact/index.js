9ff924fd124aa6e92f967ddd51ec17d88860c95a
jani <jani.behm@gmail.com>
2022-07-18 05:13:49 +0300

    contact text update

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -4,11 +4,6 @@ import styles from "../../styles/Contact.module.css";
 function IndexTest() {
   return (
     <div>
-      <div className={styles.text}>
-        <h4>Sähköposti: jani.behm@gmail.com</h4>
-        <h4>Puhelin: +358 468 990 315</h4>
-      </div>
-
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSfbWItMnUtgCmmz3nRcwuZ_VnLZ-vj0KCcluAUx2V9PqcXaOw/viewform?embedded=true"
         width="640"
